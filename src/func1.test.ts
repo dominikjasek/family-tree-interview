80f8b43c91049290ed75f1c7a1a96e0bb31a89f4
dominikjasek <dominik.jasek@customink.com>
2024-06-18 23:11:32 +0200

    disable eslint rule and apply prettier

--- src/func1.test.ts
+++ src/func1.test.ts
@@ -1,11 +1,11 @@
 import { func1 } from './func1';
 
-describe("func1", () => {
-  it("should return 123 when num is 0", () => {
-    expect(func1(0)).toBe(123)
-  })
+describe('func1', () => {
+  it('should return 123 when num is 0', () => {
+    expect(func1(0)).toBe(123);
+  });
 
-  it("should return 456 when num is not 0", () => {
-    expect(func1(1)).toBe(456)
-  })
-})
+  it('should return 456 when num is not 0', () => {
+    expect(func1(1)).toBe(456);
+  });
+});
--- src/func1.ts
+++ src/func1.ts
@@ -1,6 +1,6 @@
 export const func1 = (num: number) => {
   if (num === 0) {
-  return 123
+  return 121
   }
 
   return 456
